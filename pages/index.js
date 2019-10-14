ac15941285ed258112dbbf22a41fbdda21685559
jever <jeveryun@qq.com>
2019-10-14 11:52:29 +0800

    路由钩子

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,41 @@
 import Link from 'next/link'
 import Router from 'next/router'
 import { Button } from 'antd'
+
+const events = [
+  'routeChangeStart',
+  'routeChangeComplete',
+  'routrChangeError',
+  'beforeHistoryChange',
+  'hashChangeStart',
+  'hashChangeComplete'
+]
+
+function makeEvent(type) {
+  return (...args) => {
+    console.log(type, ...args)
+  }
+}
+
+events.forEach(event => {
+  Router.events.on(event, makeEvent(event))
+})
+
 export default () => {
   function goToTestB() {
-    Router.push({
-      pathname: '/test/b',
-      query: {
-        id: 2
-      }
-    }, '/test/b/2')
+    Router.push(
+      {
+        pathname: '/test/b',
+        query: {
+          id: 2
+        }
+      },
+      '/test/b/2'
+    )
   }
   return (
     <>
-      <Link href="/a?id=1" as="/a/1" title="AAA">
+      <Link href="/a?id=1" as="/a/1">
         <Button>a</Button>
       </Link>
       <Button onClick={goToTestB}>test b</Button>
