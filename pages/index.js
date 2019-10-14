64dad34a6bfffc314c29e6c4182bcd804c113d9b
jever <jeveryun@qq.com>
2019-10-14 11:09:58 +0800

    nextjs Link 和 Router 路由跳转

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,16 @@
+import Link from 'next/link'
+import Router from 'next/router'
 import { Button } from 'antd'
-export default () => <Button>Index</Button>
+export default () => {
+  function goToTestB() {
+    Router.push('/test/b')
+  }
+  return (
+    <>
+      <Link href="/a" title="AAA">
+        <Button>a</Button>
+      </Link>
+      <Button onClick={goToTestB}>test b</Button>
+    </>
+  )
+}
