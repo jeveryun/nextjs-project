c0b688650d39e0e3e9e05de992de8c08a519d9ce
jever <jeveryun@qq.com>
2019-10-14 14:24:34 +0800

    getInitialProps

--- pages/a.js
+++ pages/a.js
@@ -2,10 +2,20 @@ import { withRouter } from 'next/router'
 import Link from 'next/link'
 import Comp from '../components/comp'
 
-const A = ({ router }) => (
+const A = ({ router , name }) => (
   <Link href="#aaa">
-    <a>A {router.query.id}</a>
+    <a>A {router.query.id} {name}</a>
   </Link>
 )
+
+A.getInitialProps = async () => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({name: 'jokcy'})
+    }, 1000)
+  })
+
+  return await promise
+}
 
 export default withRouter(A)
