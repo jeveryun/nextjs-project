a1c607825136612b2ab9a37c66a97f8f791e921e
jever <jeveryun@qq.com>
2019-10-16 14:30:56 +0800

    useState,useEffect,userLayoutEffect,useReducer

--- pages/test/b.js
+++ pages/test/b.js
@@ -1,41 +1,50 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useReducer, useEffect, useLayoutEffect } from 'react'
 
-class MyCount extends React.Component {
-  state = {
-    count: 0
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.setState({ count: this.state.count + 1 })
-    }, 1000)
-  }
-
-  componentWillUnmount() {
-    if (this.interval) {
-      clearInterval(this.interval)
-    }
-  }
-
-  render() {
-    return <span>{this.state.count}</span>
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'add':
+      return state + 1
+    case 'minus':
+      return state - 1
+    default:
+      return state
   }
 }
 
 function MyCountFunc() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [count, dispatchCount] = useReducer(countReducer, 0)
+  const [name, setName] = useState('jever')
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     // setCount(c => c + 1)
+  //     dispatchCount({ type: 'minus' })
+  //   }, 1000)
+
+  //   return () => {
+  //     clearInterval(interval)
+  //   }
+  // }, [])
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCount(c => c + 1)
-    }, 1000)
+    console.log('effect invoked')
 
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
+    return () => console.log('effect detached')
+  }, [count])
 
-  return <span>{count}</span>
+  useLayoutEffect(() => {
+    console.log('effect layout invoked')
+
+    return () => console.log('effect layout detached')
+  }, [count])
+
+  return (
+    <div>
+      <input value={name} type="text" onChange={e => setName(e.target.value)} />
+      <button onClick={() => dispatchCount({ type: 'add' })}>{count}</button>
+    </div>
+  )
 }
 
 export default MyCountFunc
