e6ed966f0b03d2d11ea2346599c44a9d1c64593e
jever <jeveryun@qq.com>
2019-10-18 16:15:32 +0800

    reducer

--- store/store.js
+++ store/store.js
@@ -1,12 +1,17 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 
 const initialState = {
   count: 0
 }
 
+const userInitalState = {
+  username: 'jever',
+  age: 18
+}
+
 const ADD = 'ADD'
 
-function reducer(state = initialState, action) {
+function countReducer(state = initialState, action) {
   console.log(state, action)
   switch (action.type) {
     case ADD:
@@ -16,7 +21,25 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer, initialState)
+const UPDATE_USERNAME = 'UPDATE_USERNAME'
+function userReducer(state = userInitalState, action) {
+  switch (action.type) {
+    case UPDATE_USERNAME:
+      return { ...state, username: action.name }
+    default:
+      return state
+  }
+}
+
+const allReducers = combineReducers({
+  counter: countReducer,
+  user: userReducer
+})
+
+const store = createStore(allReducers, {
+  counter: initialState,
+  user: userInitalState
+})
 
 store.dispatch({ type: ADD })
 
@@ -25,5 +48,6 @@ store.subscribe(() => {
 })
 
 store.dispatch({ type: ADD })
+store.dispatch({ type: UPDATE_USERNAME, name: 'lisi'})
 
 export default store
